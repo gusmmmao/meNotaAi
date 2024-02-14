6069cd7b406482a6360f5362d158bc11cf420e91
gabrielcolegadasilva <gabrielcolegadasilva@gmail.com>
2024-02-14 20:35:38 +0000

    Backend funcionando e testes realizados pelo IDX

--- backend/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    user : "root",
-    password : "root",
-    database : "meNotaAiBdD",
-    host : "127.0.0.1",
-}
--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/test', (req, res) => {
+    res.send('Teste feito');
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
